399ce93ee330a668252ec9a7633586be9c10528e
FlavioLangHFTM <flavio.lang@hftm.ch>
2025-01-08 23:27:10 +0100

    feat: created demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatIconModule,
+    CommonModule,
+    FormsModule,
+    MatListModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'angular-blog-flavio-lang';
+  title = 'demo-app';
+  todos: { text: string; completed: boolean }[] = [];
+  newTodo = '';
+
+  addTodo() {
+    if (this.newTodo) {
+      this.todos.push({ text: this.newTodo, completed: false });
+      this.newTodo = '';
+    }
+  }
+
+  toggleTodoCompletion(todo: { text: string; completed: boolean }) {
+    todo.completed = !todo.completed;
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
