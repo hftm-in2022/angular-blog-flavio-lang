9534fec4116826065cf040a9bea12980c0fd231f
FlavioLangHFTM <flavio.lang@hftm.ch>
2025-01-09 00:40:54 +0100

    feat: implemented simple logging interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { loggingInterceptor } from './interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useValue: loggingInterceptor,
+      multi: true,
+    },
   ],
 };
