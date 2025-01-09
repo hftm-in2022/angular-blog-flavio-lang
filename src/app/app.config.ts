f10efdcf2c7f5466156459c3ab7276ee5e32416d
FlavioLangHFTM <flavio.lang@hftm.ch>
2025-01-09 01:32:13 +0100

    feat: implemented basic blog service for loading list of blog entries

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { loggingInterceptor } from './interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
     {
       provide: HTTP_INTERCEPTORS,
