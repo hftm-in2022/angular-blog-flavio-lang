8a7cdd53d3995b76e564a6f90822dddf79ecff93
laf <flavio.lang@rswag.ch>
2025-01-09 13:38:11 +0100

    feat: added empty blog-detail component and basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BlogListComponent } from './components/blog-list/blog-list.component';
-import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbarModule, BlogListComponent, MatButtonModule],
+  imports: [MatToolbarModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BlogListComponent } from './components/blog-list/blog-list.component';
+import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/blogs', pathMatch: 'full' },
+  { path: 'blogs', component: BlogListComponent },
+  { path: 'blogs/:id', component: BlogDetailComponent },
+];
