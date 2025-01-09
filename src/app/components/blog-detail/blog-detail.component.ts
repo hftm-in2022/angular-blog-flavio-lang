2954bec3ed58757ec7bfa16670e8ae33a3ecbc08
FlavioLangHFTM <flavio.lang@hftm.ch>
2025-01-09 21:53:12 +0100

    feat: completed blog detail view

--- src/app/components/blog-detail/blog-detail.component.ts
+++ src/app/components/blog-detail/blog-detail.component.ts
@@ -1,20 +1,49 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute } from '@angular/router';
 import { BlogDetailPage } from 'src/app/core/schemas/blogs.schema';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-blog-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    DatePipe,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './blog-detail.component.html',
   styleUrl: './blog-detail.component.scss',
 })
 export class BlogDetailComponent implements OnInit {
   data: BlogDetailPage | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
+  ) {}
 
   ngOnInit(): void {
     this.data = this.route.snapshot.data['data'] as BlogDetailPage;
+  }
+
+  onLikeClick() {
+    this.snackBar.open('Likes noch nicht implementiert!', 'Schliessen', {
+      duration: 3000,
+    });
+  }
+
+  onCommentClick() {
+    this.snackBar.open(
+      'Kommentare erstellen noch nicht implementiert!',
+      'Schliessen',
+      { duration: 3000 },
+    );
   }
 }
